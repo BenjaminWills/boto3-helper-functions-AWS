c91af2ad07160af7c0bb7739f7c2bc9224e57f2f
Benjaminwills <benjaminwills047@gmail.com>
2022-11-17 13:37:19 +0000

    Added cloudformation helper

---
+++ boto3_helper_classes/boto3_cloudformation_helper_class.py
@@ -0,0 +1,28 @@
+import boto3
+import logging
+import botocore.exceptions
+from typing import List
+
+class Cloudformation:
+    def __init__(self,ACCESS_KEY_ID:str,SECRET_ACCESS_KEY:str) -> None:
+        self.cloud_formation = boto3.client(
+            'cloudformation',
+            aws_access_key_id = ACCESS_KEY_ID,
+            aws_secret_access_key = SECRET_ACCESS_KEY,)
+
+    def create_stack(
+        self,
+        stack_name:str,
+        config_path:str,
+        parameters:List[dict],
+    ):
+        try:
+            self.cloud_formation.create_stack(
+                StackName = stack_name,
+                TemplateURL = config_path,
+                Parameters = parameters
+            )
+            return True, "Cloudformation Successful"
+        except botocore.Exception as e:
+            logging.error(e)
+            return False, "Cloudformation Failed."
